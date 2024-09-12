2919fe48c247b37f36b0645728cb398d22640c41
quangphucc0903 <91825763+quangphucc0903@users.noreply.github.com>
2024-09-12 10:03:09 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import {Login} from './Login/Login.js';
+import {Login} from './Login/login.js';
 import {Signup} from './Login/signup.js';
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <div>
-        <Login /> 
+        <Login />
         {/* <Signup />  */}
       </div>
     </>
