7cb4064f54c187d5f89d6a3f740e4b01204a0082
quangphucc0903 <91825763+quangphucc0903@users.noreply.github.com>
2024-10-07 14:47:52 +0700

    fix title

--- pages/admin/users.js
+++ pages/admin/users.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import DefaultLayout from './layout/DefaultLayout';
 import TableAccount from './component/table/TableAccount';
-import {DoubleRightOutlined } from '@ant-design/icons';
+import { DoubleRightOutlined } from '@ant-design/icons';
 
 const Title_Head = styled.h1`
     font-size: 32px;
@@ -11,6 +11,11 @@ const Title_Head = styled.h1`
     margin-top: 0;
     display: block;
     visibility: visible;
+
+    .icon {
+        font-size: 24px; /* Kích thước nhỏ hơn so với văn bản */
+        vertical-align: middle; /* Canh giữa theo chiều dọc với văn bản */
+    }
 `;
 
 const ContentWrapper = styled.div`
@@ -21,7 +26,9 @@ const Users = () => {
     return (
         <DefaultLayout>
             <ContentWrapper>
-                <Title_Head>Quản lí tài khoản <DoubleRightOutlined /></Title_Head>
+                <Title_Head>
+                    Quản lí tài khoản <DoubleRightOutlined className="icon" />
+                </Title_Head>
                 <TableAccount />
             </ContentWrapper>
         </DefaultLayout>
