aca14851fec7f1d041450ea313a435af140dc260
Nguyễn Minh Tú <tuem1410@gmail.com>
2024-11-07 11:15:47 +0700

    change baseUrl

--- config/request.js
+++ config/request.js
@@ -2,11 +2,12 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import QueryString from "qs";
 
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const request = axios.create({
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
   },
-  baseURL: "https://cnbe.onrender.com/api/v1",
+  baseURL: ` ${baseURL}`,
   timeout: 50000,
 });
 
@@ -16,18 +17,21 @@ request.defaults.paramsSerializer = {
   },
 };
 
-request.interceptors.request.use((config) => {
-  const token = Cookies.get('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  } else {
-    console.warn("Token không tồn tại");
+request.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      console.warn("Token không tồn tại");
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export const getToken = () => {
-  return Cookies.get('token');
+  return Cookies.get("token");
 };
